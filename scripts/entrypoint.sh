1fb2449e2fc20a79f5945e9eecb558889c27f91d
afscomercial <afscomercial@gmail.com>
2025-08-07 21:41:25 -0400

    change model

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,7 +10,7 @@ if [ ! -f "$MODEL_PATH" ]; then
 
 
   curl -L -H "Authorization: Bearer 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" \
-       https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q4_K_S.gguf \
+       https://huggingface.co/TheBloke/phi-2-GGUF/resolve/main/phi-2.Q4_0.gguf \
        --output "$MODEL_PATH"
   echo "[entrypoint] Download complete."
 fi
